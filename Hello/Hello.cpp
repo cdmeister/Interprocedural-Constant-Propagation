f1c5cafba3257c823aefe379eb8063fc4f85d0c0
Kiarash Rahbar <kdrahbar@eecs583a.eecs.umich.edu>
2015-12-09 14:53:26 -0500

    cleaned up code

--- Hello/Hello.cpp
+++ Hello/Hello.cpp
@@ -22,6 +22,9 @@
 #include "llvm/IR/Argument.h"
 #include <vector>
 #include <map>
+#include <queue>
+#include <deque>
+
 
 
 using namespace llvm;
@@ -37,74 +40,42 @@ namespace {
     Hello() : ModulePass(ID) {}
     
     private:
-     std::map <llvm::Argument*,std::vector<llvm::Argument*>> supportSet;
+     std::map <llvm::Argument*,std::vector<llvm::Argument*>> consumerSet;
     public:
     bool runOnModule(Module &M) override {
       ++HelloCounter;
-      for(Module::iterator F=M.begin(), E=M.end(); F != E ; ++F){
-        
-        errs() << "Name of Function: " << F->getName() << '\n' << '\n';
-        initSupportSets(&(*F)); 
-        bool has_callInst = false;
-        for (inst_iterator I = inst_begin(*F), E = inst_end(*F); I != E; ++I){ 
-          if (CallInst* callInst = dyn_cast<CallInst>(&*I)) {
-            has_callInst =true;
-            errs() << F->getName() << " has at least one call instruction" << '\n';
-          }
-        }
-        if(has_callInst){ 
-          Function::arg_iterator Foo_args_begin = F->arg_begin();
-          Function::arg_iterator Foo_args_end = F->arg_end();
-          for(; Foo_args_begin != Foo_args_end; ++Foo_args_begin){
-            Value * v = dyn_cast<llvm::Value>(Foo_args_begin);
-            errs() << "Arg: " << *v<< '\n';
-            std::vector<llvm::Instruction*> seen_list;
-            helper1(v,&(*F),Foo_args_begin,seen_list);   
-            seen_list.clear();    
-          }
+      initConsumerSets(M); 
 
-          
-          for (inst_iterator I = inst_begin(*F), E = inst_end(*F); I != E; ++I){
-            /*errs() << "Inst: " << *I << '\n';
-            for (Use &U : I->operands()) {
-              Value *v = U.get();
-              errs() << "Value: " << *v << '\n';
-            }*/
-            if (CallInst* callInst = dyn_cast<CallInst>(&*I)) {
-              //  We know know that this instruction is a callstie
-              errs() <<"Callsite: "<< *callInst << " " << callInst->getNumArgOperands() << '\n';
-                          CallSite CS(callInst);
-              //  IMPORTANT!!!!!!!Get the definition of the called function in llvm::Function
-              Function * test = CS.getCalledFunction();
-              errs() <<"Get Called Function: "<< *test << '\n';
-              //Loop through the arguments of the callsite parameters
-              CallSite::arg_iterator AI = CS.arg_begin();
-              CallSite::arg_iterator AE = CS.arg_end();
 
-              Function::arg_iterator FI = F->arg_begin();
-              Function::arg_iterator FE = F->arg_end();
-              for(;AI != AE; ++AI){
-                //errs() << *(callInst->getArgOperand(0)) << '\n';
-                for(;FI != FE; ++FI){
-                  Value * v_call = dyn_cast<llvm::Value>((AI));
-                  Value * v_func = dyn_cast<llvm::Value>((FI));
-                  //errs() << *FI << '\n';
-                }
-              }
-            }
-          }
+      // Print Consumer Sets
+      for(auto &formalP : consumerSet) {
+        errs() << "The consumers of " << *(formalP.first) << "\n";
+        for(auto &supp : formalP.second){
+          errs() << *supp << '\n';
         }
       }
-      //Print Consumer Sets
-     // for(auto &formalP : supportSet) {
-     //   errs() << "The consumers of " << *(formalP.first) << "\n";
-     //   for(auto &supp : formalP.second){
-     //     errs() << *supp << '\n';
-     //   }
-     // }
       return false;
+    
+      ipConstantProp(M);
     }
-    bool helper2(llvm::Instruction * inst, std::vector<llvm::Instruction *> seen_list){
+
+    void ipConstantProp(Module &M) {
+      std::deque<llvm:Argument*> worklist;
+
+      for(Module::iterator F=M.begin(), E=M.end(); F != E ; ++F){
+        Function::arg_iterator formal_param = F->arg_begin();
+        Function::arg_iterator FE = F->arg_end();
+
+        for(;formal_param != FE; ++formal_param){
+          worklist.push(formal_param);
+        }
+      }
+
+      // while(!worklist.empty()) {
+      // }
+    }
+
+    bool ifInstructionSeen(llvm::Instruction * inst, std::vector<llvm::Instruction *> seen_list){
       for(int i=0; i < seen_list.size(); ++i){
         if(inst->isIdenticalTo(seen_list[i])){
           return true;
@@ -114,24 +85,25 @@ namespace {
     }
 
 
-    void helper1(llvm::Value * v, Function * F, llvm::Argument * formal_param,
+    void getConsumers(llvm::Value * v, Function * F, llvm::Argument * formal_param,
                   std::vector<llvm::Instruction*> seen_inst){
+      
       if(Instruction * test = dyn_cast<Instruction>(v)){
-        if(helper2(test,seen_inst)){
+        if(ifInstructionSeen(test, seen_inst)){
          return;  
         }
         else{
           seen_inst.push_back(test);  
         }
       }         
+
       for( User * U : v->users()){
         if (Instruction *Inst = dyn_cast<Instruction>(U)) {
             errs() <<"Lolz " << *Inst << "\n";
 
-
-          // Base Case
+          // The instruction is a call site, so look to find the actual param
+          // that the relation exists on and add it to the formal params consumer set
           if (Inst->getOpcode() == Instruction::Call) {
-            // Doing the check
             CallSite CS(Inst);
             errs() << "CallInst: " <<*Inst << "\n";
             Instruction * previous = dyn_cast<Instruction>(v);
@@ -141,60 +113,94 @@ namespace {
             CallSite::arg_iterator AE = CS.arg_end();
             Function * defined_func = CS.getCalledFunction();
             Function::arg_iterator FI = defined_func->arg_begin();
+
             for(;AI != AE; ++AI, ++FI){
               if(Instruction * cs_arg = dyn_cast<Instruction>(*AI)){
                 if(cs_arg->isIdenticalTo(previous)){
                   errs() <<"Doing the check for prev and callsite arg" << '\n';
-                  supportSet[formal_param].push_back(FI); 
+                  consumerSet[formal_param].push_back(FI); 
                 }
               }
             }
 
-  
-
-            // Loop through the function formal parameters
-            // This will be the key
-      
-            
             return;
           }
+
           else if(Inst->getOpcode() == Instruction::Ret){
              errs() << "Exiting\n";
             return;
           } 
+
           if(Inst->getOpcode() == Instruction::Store){
             Value * operand = dyn_cast<Value>(Inst->getOperand(1));
-            //errs() <<"Store operand: "<< *operand << '\n';
-            helper1(operand,F,formal_param,seen_inst);    
+            getConsumers(operand,F,formal_param,seen_inst);    
           }
+
           else if(Inst->getOpcode() == Instruction::Load){
-             Value * load = dyn_cast<Value>(Inst);
+            Value * load = dyn_cast<Value>(Inst);
             
-            //errs() <<"Load operand: "<< *operand << '\n';
-            helper1(load,F,formal_param,seen_inst);   
+            getConsumers(load,F,formal_param,seen_inst);   
           }
           
           else{
             //Value * v1 = dyn_cast<Value>(U);
             Value * other = dyn_cast<Value>(Inst);
-            helper1(other,F,formal_param,seen_inst);
+            getConsumers(other,F,formal_param,seen_inst);
             
           }
         }
         else{
           return;
-          errs() << "Huh" << '\n';
         }
       }         
      }
+    
+    // Initialize every formal parameter of every function with an empty vector of consumers  
+    void initConsumerSets(Module &M){
+      for(Module::iterator F=M.begin(), E=M.end(); F != E ; ++F){
+        Function::arg_iterator FI = F->arg_begin();
+        Function::arg_iterator FE = F->arg_end();
+
+        for(;FI != FE; ++FI){
+          std::vector<llvm::Argument*> new_vector;
+          consumerSet[FI]=new_vector;
+        }
+      }
       
-    void initSupportSets(Function * F){
-      Function::arg_iterator FI = F->arg_begin();
-      Function::arg_iterator FE = F->arg_end();
-      //  Loop through every formal parameter of the function and make an empty vector
-      for(;FI != FE; ++FI){
-        std::vector<llvm::Argument*> new_vector;
-        supportSet[FI]=new_vector;
+      for(Module::iterator F=M.begin(), E=M.end(); F != E ; ++F){
+        
+        errs() << "Name of Function: " << F->getName() << '\n' << '\n';
+        
+        bool has_callInst = false;
+        for (inst_iterator I = inst_begin(*F), E = inst_end(*F); I != E; ++I){ 
+          if (CallInst* callInst = dyn_cast<CallInst>(&*I)) {
+            has_callInst =true;
+            errs() << F->getName() << " has at least one call instruction" << '\n';
+          }
+        }
+
+        if(has_callInst){ 
+          Function::arg_iterator Foo_args_begin = F->arg_begin();
+          Function::arg_iterator Foo_args_end = F->arg_end();
+          for(; Foo_args_begin != Foo_args_end; ++Foo_args_begin){
+            Value * v = dyn_cast<llvm::Value>(Foo_args_begin);
+            errs() << "Arg: " << *v<< '\n';
+            std::vector<llvm::Instruction*> seen_list;
+            getConsumers(v,&(*F),Foo_args_begin,seen_list);   
+            seen_list.clear();    
+          }
+
+          // Print out function body if it contains a callsite
+          for (inst_iterator I = inst_begin(*F), E = inst_end(*F); I != E; ++I){
+            if (CallInst* callInst = dyn_cast<CallInst>(&*I)) {
+              errs() <<"Callsite: "<< *callInst << " " << callInst->getNumArgOperands() << '\n';
+                          CallSite CS(callInst);
+              Function * test = CS.getCalledFunction();
+
+              errs() <<"Get Called Function: "<< *test << '\n';
+            }
+          }
+        }
       }
     }
   };
