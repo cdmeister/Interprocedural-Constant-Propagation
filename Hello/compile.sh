ad96917bcc7eb25e3e1e50dbcb09cdab74936dfc
Moeiz Riaz <moeizr@umich.edu>
2015-12-09 17:00:03 -0500

    adding constprop pass to compile script

--- Hello/compile.sh
+++ Hello/compile.sh
@@ -10,7 +10,9 @@ llc test/test.bc -o test/test.s
 g++ -o test/test test/test.s 
 ./test/test 
 
-opt -stats -load=../lib/Hello.so -hello test/test.bc > test/test.ipco.bc
+opt -stats -constprop test/test.bc > test/test.const.bc
+opt -stats -load=../lib/Hello.so -hello test/test.const.bc > test/test.ipco.bc
+llvm-dis test/test.ipco.bc
 
 #llc test/test.slicm.bc -o test/test.slicm.s
 #g++ -o test/test test/test.slicm.s 
